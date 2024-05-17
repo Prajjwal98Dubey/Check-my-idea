b1c8ce1ae47caf30981b5fad61698e90c7b8c46c
Prajjwal98Dubey <prajjwaldubey003@gmail.com>
2024-05-17 15:47:41 +0530

    Fixed BUG - same search query is being added to the user history for thant one time.

--- server/config/searchHistory.js
+++ server/config/searchHistory.js
@@ -4,40 +4,44 @@ const Product = require('../models/productSchema')
 const historyRouter = express.Router()
 const redis = require('redis')
 // -> "key-32dj23uq9weid-search-history"
-const addSearchedQueryToRedis=async(req,res)=>{
-        let redisClient = await redis.createClient({})
-        await redisClient.connect()
-        const {email,searchQuery} = req.body
-        const product = await Product.findOne({_id:searchQuery})
-        const user  = await User.findOne({email:email})
-        const userId = user._id;
-        let response = [];
-        let userSearchHistory = await redisClient.GET(`key-${userId}-search-history`)
-        if(userSearchHistory){
-            const parsedUserSearchHistoryList = JSON.parse(userSearchHistory)
-            parsedUserSearchHistoryList.push(product)
-            await redisClient.SET(`key-${userId}-search-history`,JSON.stringify(parsedUserSearchHistoryList))
-            response = parsedUserSearchHistoryList
+const addSearchedQueryToRedis = async (req, res) => {
+    let redisClient = await redis.createClient({})
+    await redisClient.connect()
+    const { email, searchQuery } = req.body
+    const product = await Product.findOne({ _id: searchQuery })
+    const user = await User.findOne({ email: email })
+    const userId = user._id;
+    let response = [];
+    let userSearchHistory = await redisClient.GET(`key-${userId}-search-history`)
+    if (userSearchHistory) {
+        const parsedUserSearchHistoryList = JSON.parse(userSearchHistory)
+        let flag = 1;
+        for (let i = 0; i < parsedUserSearchHistoryList.length; i++) {
+            if (parsedUserSearchHistoryList[i]._id === searchQuery) flag = 0
         }
-        else{
-            let userSearchHistoryList = [product];
-            await redisClient.SET(`key-${userId}-search-history`,JSON.stringify(userSearchHistoryList))
-            response = userSearchHistoryList
-        }
-        res.json(response)
+        if (flag) parsedUserSearchHistoryList.push(product)
+        await redisClient.SET(`key-${userId}-search-history`, JSON.stringify(parsedUserSearchHistoryList))
+        response = parsedUserSearchHistoryList
+    }
+    else {
+        let userSearchHistoryList = [product];
+        await redisClient.SET(`key-${userId}-search-history`, JSON.stringify(userSearchHistoryList))
+        response = userSearchHistoryList
+    }
+    res.json(response)
 }
 // -> "key-32dj23uq9weid-search-history"
 
-const getMySearchHistory=async(req,res)=>{
+const getMySearchHistory = async (req, res) => {
     let redisClient = await redis.createClient({})
     await redisClient.connect()
     const email = req.query.email
-    const user = await User.findOne({email:email})
+    const user = await User.findOne({ email: email })
     const userId = user._id
     const userKeyInRedis = await redisClient.GET(`key-${userId}-search-history`)
     let response = [];
-    if (userKeyInRedis){
-            response = JSON.parse(userKeyInRedis).reverse().slice(0,3);
+    if (userKeyInRedis) {
+        response = JSON.parse(userKeyInRedis).reverse().slice(0, 3);
     }
     res.json(response)
 
